3b8677312306379020c36b774bbfbf39c085a7be
Madsen W. J. Servius <madsen@madsendynamics.com>
2022-03-07 08:09:59 -0500

    refactoring

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-from setuptools import setup 
+from setuptools import setup, find_packages
 
 setup(
     name='moncash',
     version='1.0.3',
+    license='MIT',
     description='Simple python wrapper to perform and retrieve payment to Digicel Moncash with Python',
+    packages=find_packages('src'),
+    package_dir={'': 'src'}, 
     author="Dokla",
     author_email="support@dokla.ht",
     install_requires=["requests>=2.25.1,<3.0"],
